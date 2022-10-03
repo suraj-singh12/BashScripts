18942ff2b24b5df7748c18b9fb9c677fe1b78061
MohammedRoshanT <70361124+MohammedRoshanT@users.noreply.github.com>
2022-10-03 09:51:25 +0530

    passsword generator

---
+++ password generator/pwgen.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+echo "Welcome to Password Generator"
+echo ""
+echo ""
+
+
+echo "Creating 14 digit password..."
+echo ""
+pwgen 14 1
---
+++ password generator/readme.md
@@ -0,0 +1,11 @@
+# 14 digit password generator
+
+### Requirement
+ sudo apt install pwgen -y
+
+### Usage 
+
+bash pwgen.sh
+
+
+That's all !
